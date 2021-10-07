3356b604d393079a99966bd5112ca9ee4cd2cf25
lakuzn <lakuzn003@gmail.com>
2021-10-07 23:42:04 +0300

    first commit

---
+++ 6.py
@@ -0,0 +1,26 @@
+print("Введите размер массива.")
+g = int(input())
+a = [0] * g
+for i in range(len(a)):
+    i = str(i + 1)
+    print("Введите элемент массива " + i)
+    i = int(i)
+    i = i - 1
+    a[i] = int(input())
+min = a[1]
+for i in range(len(a)):
+  if (a[i]<min):
+    min = a[i]
+print("Минимальное значение массива =",min)
+print("Введите число DELTA")
+delta = int(input())
+x = min - delta
+y = min + delta
+t = 0
+for i in range(len(a)):
+    if (a[i]==x) or (a[i]==y):
+        t = t + 1;
+if (t==0):
+    print("Элементов  в  заданном  массиве, отличающихся от минимального на DELTA - нет.")
+else:
+    print("Количество таких чисел =", t)
